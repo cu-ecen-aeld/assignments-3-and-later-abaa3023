a6ffbe392600cfc46fbaab5776c0b88b8b6ed30a
abaa3023 <abaa3023@colorado.edu>
2022-09-10 22:00:56 -0600

    Initial changes in manual-linux.sh, facing issues with cross-compiler

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,7 +35,13 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
-fi
+    # qemu-system-arm -m 256M -nographic -M versatilepb -kernel zImage -append "console=ttyAMA0 rdinit=/bin/sh" -dtb versatile-pb.dtb -initrd initramfs.cpio.gz
+    sudo make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    sudo make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    sudo make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    sudo make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    sudo make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+fi 
 
 echo "Adding the Image in outdir"
 
@@ -48,6 +54,15 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs
+cd "$OUTDIR/rootfs"
+mkdir bin dev etc home lib proc sbin sys tmp usr var
+mkdir usr/bin usr/lib usr/sbin
+mkdir -p var/log
+
+cd "$OUTDIR"
+sudo chown -R root:root *
+
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +70,46 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    # TODO:  Configure busybox 
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+sudo make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
-
+cd "$OUTDIR/rootfs"
+cp -a $SYSROOT/lib/ld-linux-armhf.so.3 lib
+cp -a $SYSROOT/lib/ld-2.22.so lib
+cp -a $SYSROOT/lib/libc.so.6 lib
+cp -a $SYSROOT/lib/libc-2.22.so lib
+cp -a $SYSROOT/lib/libm.so.6 lib
+cp -a $SYSROOT/lib/libm-2.22.so lib
 # TODO: Make device nodes
-
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
+#sudo make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} INSTALL_MOD_PATH=${OUTDIR}/rootfs modules_install
 # TODO: Clean and build the writer utility
-
+cd "~/assignment-1-abaa3023/finder-app"
+make CROSS_COMPILE=${CROSS_COMPILE} clean
+make CROSS_COMPILE=${CROSS_COMPILE} all
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-
+cp ~/assignment-1-abaa3023/finder-app/finder.sh ${OUTDIR}/rootfs
+cp ~/assignment-1-abaa3023/finder-app/finder-test.sh ${OUTDIR}/rootfs
 # TODO: Chown the root directory
-
+cd "$OUTDIR"
+sudo chown -R root:root *
 # TODO: Create initramfs.cpio.gz
+cd "$OUTDIR/rootfs"
+find . | cpio -H newc -ov --owner root:root > ../initramfs.cpio
+cd ..
+gzip initramfs.cpio
+mkimage -A ${CROSS_COMPILE} -O linux -T ramdisk -d iniramfs.cpio.gz uRamdisk
