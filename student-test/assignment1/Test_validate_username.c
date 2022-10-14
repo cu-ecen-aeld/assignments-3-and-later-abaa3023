010fe25038d10952167935854560d4ffbaf4ad4c
abaa3023 <abaa3023@colorado.edu>
2022-10-13 23:55:38 -0600

    one

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -5,7 +5,7 @@
 #include "../../assignment-autotest/test/assignment1/username-from-conf-file.h"
 
 /**
-* This function should
+* This function should:
 *   1) Call the my_username() function in autotest-validate.c to get your hard coded username.
 *   2) Obtain the value returned from function malloc_username_from_conf_file() in username-from-conf-file.h within
 *       the assignment autotest submodule at assignment-autotest/test/assignment1/
